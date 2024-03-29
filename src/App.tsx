504daa4e79655da7d0a922064f5be76c1140b7b1
Rafael Siqueira <rafaelsiqueira.dev@gmail.com>
2024-03-29 10:42:53 -0300

    feat: create board store and component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
+import { useEffect } from "react"
+import { useBoardStore } from "./hooks/useBoardStore"
+import { Button } from "./components/ui/button"
+
 function App() {
-  return <h1 className="font-bold">App</h1>
+  const { board, resetBoard, clickPiece } = useBoardStore()
+
+  useEffect(() => {
+    resetBoard(10, 10, 20)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <div className="flex justify-center w-1/2 mx-auto mt-4">
+      <div className="grid grid-cols-10 gap-1 bg-slate-500 p-1">
+        {board.map((piece, index) =>
+          piece.clicked ? (
+            <Button
+              variant="secondary"
+              className="size-10 rounded-none"
+              key={index}
+              disabled
+            >
+              {piece.value}
+            </Button>
+          ) : (
+            <Button
+              className="size-10 rounded-none relative"
+              onClick={() => clickPiece(index)}
+              key={index}
+            ></Button>
+          )
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ src/hooks/useBoardStore.ts
@@ -0,0 +1,120 @@
+import { create } from "zustand"
+
+type Board = Piece[]
+
+type Piece = {
+  value: number
+  clicked: boolean
+}
+
+interface BoardStore {
+  board: Board
+  x: number
+  y: number
+  bombs: number
+  resetBoard: (x: number, y: number, bombs: number) => void
+  clickPiece: (index: number) => void
+}
+
+export const useBoardStore = create<BoardStore>()((set, get) => ({
+  board: [],
+  x: 0,
+  y: 0,
+  bombs: 0,
+  resetBoard: (x, y, bombs) =>
+    set({ board: createBoard(x, y, bombs), x, y, bombs }),
+  clickPiece: (index) => set({ board: recursiveClickPiece(get(), index) }),
+}))
+
+function createBoard(x: number, y: number, bombs: number): Board {
+  let boardStr = "0".repeat(x * y)
+  const bombsPositions = getRandomNumbers(bombs, x * y)
+
+  bombsPositions.forEach((pos) => {
+    boardStr = changeCharacterAtIndex(boardStr, pos, "b")
+  })
+
+  const boardWithBombs: Board = boardStr.split("").map((value) => ({
+    value: value === "b" ? -1 : Number(value),
+    clicked: false,
+  }))
+
+  const board = setNeighborsValue(boardWithBombs, bombsPositions, x, y)
+
+  return board
+}
+
+function recursiveClickPiece(data: BoardStore, index: number): Piece[] {
+  const { board, x, y } = data
+
+  board[index].clicked = true
+
+  const recall = (index: number) => {
+    if (board[index].clicked === false) {
+      recursiveClickPiece(data, index)
+    }
+  }
+
+  if (board[index].value === 0) {
+    if (index - x - 1 >= 0 && index % x > 0) recall(index - x - 1) // top left
+    if (index - x >= 0) recall(index - x) // top
+    if (index - x + 1 > 0 && index % x < x - 1) recall(index - x + 1) // top right
+    if ((index - 1) % x >= 0 && index % x !== 0) recall(index - 1) // left
+    if ((index + 1) % x >= 0 && index % x !== x - 1) recall(index + 1) // right
+    if (Math.floor(index / x) < y - 1 && index % x > 0) recall(index + x - 1) // bottom left
+    if (Math.floor(index / x) < y - 1) recall(index + x) // bottom
+    if (Math.floor(index / x) < y - 1 && index % x < x - 1)
+      recall(index + x + 1) // bottom right
+  }
+
+  return board
+}
+
+function setNeighborsValue(
+  boardWithBombs: Board,
+  bombsPositions: number[],
+  x: number,
+  y: number
+): Board {
+  const board: Board = boardWithBombs
+
+  const add = (index: number) => {
+    if (board[index].value !== -1) {
+      board[index].value++
+    }
+  }
+
+  bombsPositions.forEach((index) => {
+    if (index - x - 1 >= 0 && index % x > 0) add(index - x - 1) // top left
+    if (index - x >= 0) add(index - x) // top
+    if (index - x + 1 > 0 && index % x < x - 1) add(index - x + 1) // top right
+    if ((index - 1) % x >= 0 && index % x !== 0) add(index - 1) // left
+    if ((index + 1) % x >= 0 && index % x !== x - 1) add(index + 1) // right
+    if (Math.floor(index / x) < y - 1 && index % x > 0) add(index + x - 1) // bottom left
+    if (Math.floor(index / x) < y - 1) add(index + x) // bottom
+    if (Math.floor(index / x) < y - 1 && index % x < x - 1) add(index + x + 1) // bottom right
+  })
+
+  return board
+}
+
+// HELPERS
+function getRandomNumbers(bombs: number, size: number) {
+  const numbers: number[] = []
+
+  while (numbers.length < bombs) {
+    const randomNumber = Math.floor(Math.random() * size)
+
+    if (!numbers.includes(randomNumber)) {
+      numbers.push(randomNumber)
+    }
+  }
+
+  return numbers
+}
+
+function changeCharacterAtIndex(str: string, index: number, newChar: string) {
+  const chars = str.split("")
+  chars[index] = newChar
+  return chars.join("")
+}
